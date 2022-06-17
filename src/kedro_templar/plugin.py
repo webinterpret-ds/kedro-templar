70177dd05fcfa798bf5ddbcad666f6a352eac353
Adrian <adrian.bogucki@webinterpret.com>
2022-06-17 17:43:42 +0200

    added all scripts

---
+++ src/kedro_templar/plugin.py
@@ -0,0 +1,19 @@
+import click
+import process_templates as pt
+import logging
+
+log = logging.getLogger(__name__)
+
+
+@click.group(name="JSON")
+def commands():
+    """Kedro plugin for printing the pipeline in JSON format"""
+    pass
+
+
+@commands.command()
+@click.pass_obj
+def templar(metadata):
+    """Run script that fills templates"""
+    pt.process_templates()
+    log.info('Plugin templar finished run')
