617728e6ec4f39547f33611393b7b3a411bdd3a8
Adrian <adrian.bogucki@webinterpret.com>
2022-06-21 14:40:50 +0200

    some changes

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 creates a structure of config based on parameters
 example of folder structure is placed in examples folder
+
+
+to connect to AWS s3 please create a Environment variable
+AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY
+AWS_DEFAULT_REGION
+using export
+eg export AWS_DEFAULT_REGION=eu-west-2
--- src/kedro_templar/process_templates.py
+++ src/kedro_templar/process_templates.py
@@ -2,7 +2,8 @@ import logging
 import os
 from pathlib import Path
 from typing import Text, Dict
-
+import boto3
+from botocore.exceptions import ClientError
 import click
 import yaml
 from jinja2 import Environment, FileSystemLoader
@@ -13,7 +14,9 @@ log = logging.getLogger(__name__)
 # settings.py
 TEMPLATES_DIR = Path('./examples/base')
 OUTPUT_DIR = Path("./conf/base")
-
+aws_access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+aws_secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+aws_region = os.environ.get('AWS_DEFAULT_REGION')
 
 # lib/templates.p
 def create_environment(templates_dir: Text) -> Environment:
@@ -54,6 +57,29 @@ def save_result(result: Text, output_filepath: Path, replace: bool=True):
 
     with open(output_filepath, "w") as output:
         output.write(result)
+
+
+def upload_file_to_s3(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
 
 
 # main.py
@@ -104,7 +130,7 @@ def process_templates(
     replace_existing: bool,
     config_source: Text
 ):
-
+    upload_
     config = load_config( config_source if config_source else config_path)
     environment = create_environment(templates_dir)
     for template_path in environment.list_templates():
