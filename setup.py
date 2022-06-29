0125cc1f30bc753fd9644737d12756c5dcfc6238
Wojciech Włodarczyk <heolin123@gmail.com>
2022-06-29 14:47:17 +0200

    Added requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,28 @@
+import codecs
+import os
+
 import setuptools
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def path_in_project(*path):
+    return os.path.join(here, *path)
+
+
+def read_file(filename):
+    with codecs.open(path_in_project(filename)) as f:
+        return f.read()
+
+
+def read_requirements(filename):
+    contents = read_file(filename).strip('\n')
+    return contents.split('\n') if contents else []
 
 
 setuptools.setup(
     name='kedro_templar',
     test_suite='tests',
-    install_requires=[],
+    install_requires=read_requirements('requirements.txt'),
     entry_points={"kedro.project_commands": ["kedro_templar = kedro_templar.plugin:commands"]}
 )
