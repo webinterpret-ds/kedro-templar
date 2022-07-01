3d0dbe95c948964e216e7709fe48372f75ed5f04
Wojciech <heolin123@gmail.com>
2022-07-01 17:03:38 +0200

    Added support for direct S3 paths (#4)
    * Added support for direct S3 paths

--- src/kedro_templar/settings.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import os
-
-"""
-TEMPLATES_DIR = Path(os.environ['TEMPLATES_DIR'])
-OUTPUT_DIR = Path(os.environ['OUTPUT_DIR'])
-CONFIG_OUTPUT_DIR = Path(os.environ['CONFIG_OUTPUT_DIR'])
-DEFAULT_CONFIG = Path(os.environ['DEFAULT_CONFIG'])
-"""
